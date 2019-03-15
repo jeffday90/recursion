40fcc5a5cb5a7ef141f23b1235fa6032d8b80159
Jeff Day <jeffrey.day00@gmail.com>
2019-03-15 14:03:30 -0700

    Compelted JSON.stringify from scratch - deleted some extra comments and whitespace

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,29 +4,15 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-
-// recursively call stringifyJSON until its broken into a primitive type that can be returned into a string
-
-//look through JSON.stringify mdn and try and figure out the behaviors of the data types (base case)
-
-//for arrays and objects figure out the recursive call 
-    
-//if undefined in passed in return null
   if (typeof obj === 'undefined' || obj === undefined || typeof obj === 'function' || typeof obj === 'null' || obj === null || typeof obj === 'symbol') {
     return 'null';
   }
-
-//if a boolean is passed in as obj
   if (typeof obj === 'boolean' || typeof obj === 'number') {
     return obj.toString();
   }
-
-//if string is passed in as obj
   if (typeof obj === 'string') {
     return `"${obj}"`;
   }
-
-//array
   if (Array.isArray(obj)) {
     var newArr = [];
 
@@ -35,7 +21,6 @@ var stringifyJSON = function(obj) {
     }
     return `[${newArr.toString()}]`;
   }
-//obj = object
   if (typeof obj === 'object' && obj !== null) {
     var objArr = [];
     for (key in obj) {
@@ -43,7 +28,6 @@ var stringifyJSON = function(obj) {
         objArr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
       }
     }
-
     return `{${objArr.join(',')}}`;
   }    
 };
